e3ef977614118b299dfbc2ef7540353cb4b08653
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-04-08 23:06:18 -0500

    testing that params are stored in req.params

--- test/app.params.js
+++ test/app.params.js
@@ -1,8 +1,9 @@
 import {describe, it} from "node:test";
 import expressClone from "../index.js";
 import request from "supertest";
+import assert from "node:assert";
 
-describe('app.get("/:foo")', () => {
+describe('app.get("/:foo", ...)', () => {
     const app = expressClone()
     app.get("/:foo", (req, res) => {
         res.end("hello")
@@ -23,5 +24,55 @@ describe('app.get("/:foo")', () => {
             .get("/bar/foo")
             .expect(404)
             .end(done)
+    });
+});
+
+describe('app.get', () => {
+    describe('("/:foo", ...)', () => {
+        it('should create a key foo in req.params and store its value as test', (t, done) => {
+            const app = expressClone()
+            app.get("/:foo", (req, res) => {
+                res.end(req.params.foo)
+            })
+            request(app)
+                .get("/test")
+                .expect("test")
+                .end(done);
+        });
+    });
+    describe('("/:foo/:bar", ...)', () => {
+        it('should have an Object.keys length of 2 for req.params', (t, done) => {
+            const app = expressClone()
+            app.get("/:foo/:bar", (req, res) => {
+                res.end(Object.keys(req.params).length.toString())
+            })
+            request(app)
+                .get("/test/test2")
+                .expect("2")
+                .end(done)
+        });
+        it('should store test for req.params.foo and test2 for req.params.bar', (t, done) => {
+            const app = expressClone()
+            app.get("/:foo/:bar", (req, res) => {
+                res.write(req.params.foo)
+                res.end(req.params.bar)
+            })
+            request(app)
+                .get("/test/test2")
+                .expect("testtest2")
+                .end(done)
+        });
+    });
+    describe('("/:foo/bar", ...)', () => {
+        it('should have an Object.keys length of 1 for req.params', (t, done) => {
+            const app = expressClone()
+            app.get("/:foo/bar", (req, res) => {
+                res.end(Object.keys(req.params).length.toString())
+            })
+            request(app)
+                .get("/test/bar")
+                .expect("1")
+                .end(done)
+        });
     });
 });
