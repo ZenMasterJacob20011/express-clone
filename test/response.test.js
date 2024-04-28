dcbb2a68f42bf85373243c4e100ec7c857b48792
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2024-04-28 08:38:22 -0500

    Tests for response.js

---
+++ test/response.test.js
@@ -0,0 +1,82 @@
+import {describe, it} from "node:test";
+import expressClone from "../index.js";
+import request from "supertest";
+
+describe('res.', () => {
+    describe('sendStatus()', () => {
+        it('should send OK in response body if passed 200', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.sendStatus(200)
+            })
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('OK', done)
+        });
+        it('should send status code if unknown status code is passed', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.sendStatus(787)
+            })
+            request(app)
+                .get('/')
+                .expect(787)
+                .expect('787', done)
+        });
+    });
+    describe('type()', () => {
+        it('should set the Content-Type to application/json if passed .json', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.type('.json')
+                res.end('{}')
+            })
+            request(app)
+                .get('/')
+                .expect('Content-Type', 'application/json', done)
+        });
+        it('should set the Content-Type to application/json if passed json', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.type('json')
+                res.end('{}')
+            })
+            request(app)
+                .get('/')
+                .expect('Content-Type', 'application/json', done)
+        });
+    });
+    describe('set()', () => {
+        it('should add charset to content-type', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.set('Content-Type', 'application/json')
+                res.end()
+            })
+            request(app)
+                .get('/')
+                .expect('Content-Type', 'application/json; charset=utf-8', done)
+        });
+        it('should allow multiple values to be set on one field', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.set('Accept', ['application/json', 'json'])
+                res.end()
+            })
+            request(app)
+                .get('/')
+                .expect('Accept', 'application/json, json', done)
+        });
+        it('should set the header given field and value', (t, done) => {
+            const app = expressClone()
+            app.get('/', (req, res) => {
+                res.set('Content-Length', 1)
+                res.end()
+            })
+            request(app)
+                .get('/')
+                .expect('Content-Encoding', 'gzip', done)
+        });
+    })
+});
